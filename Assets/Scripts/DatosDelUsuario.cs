545dc6907b4a8a5a773bf886425878cf5cef146c
Axel-Wolkowicz <wolkowiczaxel@gmail.com>
2026-04-16 12:09:07 -0300

    Ejercicios 1 y 2

---
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatosDelUsuario : MonoBehaviour
+{
+    public string nombre = "Axel";
+    public int edad = 15;
+    public float estatura = 1.80f;
+    public bool donante = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Mi nombre es "+ nombre + ", tengo " + edad + ", mido " + estatura + " metros y es " + donante + " que soy donante de órganos.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeclaracionDeVariables : MonoBehaviour
+{
+    public int entero = 1;
+    public float flotante = 1.45f;
+    public string texto = "hola";
+    public bool boleano = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(entero);
+        Debug.Log(flotante);
+        Debug.Log(texto);
+        Debug.Log(boleano);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
